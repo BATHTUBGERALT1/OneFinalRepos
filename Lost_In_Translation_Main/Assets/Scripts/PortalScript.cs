b255e3d03f7990f1eb077e3ef7c2d2ca128251bc
BATHTUBGERALT1 <43067557+BATHTUBGERALT1@users.noreply.github.com>
2019-05-08 02:51:41 +0100

    added the book, animated book, level 2 and portal

---
+++ Lost_In_Translation_Main/Assets/Scripts/PortalScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// extra using statemet to allow us to use scene management functions
+using UnityEngine.SceneManagement;
+public class PortalScript : MonoBehaviour {
+    
+    public string sceneToLoad;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        //checks the playerscript has been collided with
+        Player playerScript = collision.collider.GetComponent<Player>();
+
+        // something only happens when the play is hit
+        if (playerScript != null)
+        {
+            // we DID hit the player !!!!!!
+
+            
+
+      
+            //goes to the high score screen
+            SceneManager.LoadScene(sceneToLoad);
+
+        }
+
+    }
+}
