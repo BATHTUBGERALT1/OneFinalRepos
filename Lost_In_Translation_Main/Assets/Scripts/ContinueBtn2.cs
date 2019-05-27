0cb7ab308a53843349d69786e0b939947e18483c
BATHTUBGERALT1 <43067557+BATHTUBGERALT1@users.noreply.github.com>
2019-05-27 10:42:33 +0100

    final fixes

---
+++ Lost_In_Translation_Main/Assets/Scripts/ContinueBtn2.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class ContinueBtn2 : MonoBehaviour {
+
+    public void ContinueGame()
+    {
+
+        //this will load the highscore screen 
+        SceneManager.LoadScene("HighScore");
+        //reset the lives 
+        PlayerPrefs.DeleteKey("lives");
+
+
+    }
+
+}
